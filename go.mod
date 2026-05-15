8d4335ac3e29d2e19d354ef187b421ee000b0b4e
caoxudong <caoxudong818@gmail.com>
2026-05-15 20:24:17 +0800

    add api inplmentations

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module DeviceManagementPlatform-api
 go 1.26.2
 
 require (
-	github.com/KitHub/protocols v0.1.2
+	buf.build/go/protovalidate v1.2.0
+	github.com/KitHub/protocols v0.1.3
 	github.com/go-sql-driver/mysql v1.10.0
 	go.yaml.in/yaml/v3 v3.0.4
 	google.golang.org/grpc v1.81.1
@@ -13,16 +14,19 @@ require (
 
 require (
 	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.36.11-20260415201107-50325440f8f2.1 // indirect
+	cel.dev/expr v0.25.1 // indirect
 	filippo.io/edwards25519 v1.2.0 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
 	github.com/goccy/go-json v0.10.6 // indirect
 	github.com/golang/snappy v1.0.0 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/google/cel-go v0.28.0 // indirect
 	github.com/syndtr/goleveldb v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20250813145105-42675adae3e6 // indirect
 	golang.org/x/net v0.54.0 // indirect
 	golang.org/x/sys v0.44.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20260226221140-a57be14db171 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260511170946-3700d4141b60 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	xorm.io/builder v0.3.13 // indirect
 )
--- service/api_service.go
+++ service/api_service.go
@@ -3,8 +3,10 @@ package service
 import (
 	"DeviceManagementPlatform-api/logic"
 	"context"
+	"log/slog"
 	"sync"
 
+	"buf.build/go/protovalidate"
 	"github.com/KitHub/protocols/devicemanagementplatformapi"
 )
 
@@ -27,16 +29,79 @@ func NewApiService(ctx context.Context,
 }
 
 // QueryDeviceById implements [devicemanagementplatformapi.DeviceManagementPlatformAPIServer].
-func (a *ApiService) QueryDeviceById(context.Context, *devicemanagementplatformapi.QueryDeviceByIdRequest) (*devicemanagementplatformapi.QueryDeviceByIdResponse, error) {
-	panic("unimplemented")
+func (a *ApiService) QueryDeviceById(ctx context.Context, req *devicemanagementplatformapi.QueryDeviceByIdRequest) (rsp *devicemanagementplatformapi.QueryDeviceByIdResponse, err error) {
+	slog.InfoContext(ctx, "Querying device by ID", slog.Any("deviceId", req.GetDeviceId()))
+	if err = protovalidate.Validate(req); err != nil {
+		slog.ErrorContext(ctx, "Validation failed for QueryDeviceByIdRequest", "error", err)
+		return nil, err
+	}
+	deviceInfo, err := a.deviceLogic.GetDeviceById(ctx, req.GetDeviceId())
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to query device by ID", slog.Any("error", err))
+		return nil, err
+	}
+
+	rsp = &devicemanagementplatformapi.QueryDeviceByIdResponse{
+		Data: &devicemanagementplatformapi.QueryDeviceByIdResponseData{
+			DeviceInfo: &devicemanagementplatformapi.DeviceInfo{
+				DeviceId:     deviceInfo.Id,
+				DeviceNo:     deviceInfo.DeviceNo,
+				RegisterTime: deviceInfo.RegisterTime.UnixMilli(),
+			},
+		},
+	}
+	slog.InfoContext(ctx, "Successfully queried device by ID", slog.Any("device", deviceInfo))
+	return rsp, nil
 }
 
 // QueryDeviceByNo implements [devicemanagementplatformapi.DeviceManagementPlatformAPIServer].
-func (a *ApiService) QueryDeviceByNo(context.Context, *devicemanagementplatformapi.QueryDeviceByNoRequest) (*devicemanagementplatformapi.QueryDeviceByNoResponse, error) {
-	panic("unimplemented")
+func (a *ApiService) QueryDeviceByNo(ctx context.Context, req *devicemanagementplatformapi.QueryDeviceByNoRequest) (rsp *devicemanagementplatformapi.QueryDeviceByNoResponse, err error) {
+	slog.InfoContext(ctx, "Querying device by NO", slog.Any("deviceNo", req.GetDeviceNo()))
+	if err = protovalidate.Validate(req); err != nil {
+		slog.ErrorContext(ctx, "Validation failed for QueryDeviceByNoRequest", "error", err)
+		return nil, err
+	}
+	deviceInfo, err := a.deviceLogic.GetDeviceByNo(ctx, req.GetDeviceNo())
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to query device by NO", slog.Any("error", err))
+		return nil, err
+	}
+
+	rsp = &devicemanagementplatformapi.QueryDeviceByNoResponse{
+		Data: &devicemanagementplatformapi.QueryDeviceByNoResponseData{
+			DeviceInfo: &devicemanagementplatformapi.DeviceInfo{
+				DeviceId:     deviceInfo.Id,
+				DeviceNo:     deviceInfo.DeviceNo,
+				RegisterTime: deviceInfo.RegisterTime.UnixMilli(),
+			},
+		},
+	}
+	slog.InfoContext(ctx, "Successfully queried device by NO", slog.Any("device", deviceInfo))
+	return rsp, nil
 }
 
 // RegisterDevice implements [devicemanagementplatformapi.DeviceManagementPlatformAPIServer].
-func (a *ApiService) RegisterDevice(context.Context, *devicemanagementplatformapi.RegisterDeviceRequest) (*devicemanagementplatformapi.RegisterDeviceResponse, error) {
-	panic("unimplemented")
+func (a *ApiService) RegisterDevice(ctx context.Context, req *devicemanagementplatformapi.RegisterDeviceRequest) (rsp *devicemanagementplatformapi.RegisterDeviceResponse, err error) {
+	slog.InfoContext(ctx, "Registering device", slog.Any("deviceNo", req.GetDeviceNo()))
+	if err = protovalidate.Validate(req); err != nil {
+		slog.ErrorContext(ctx, "Validation failed for RegisterDeviceRequest", "error", err)
+		return nil, err
+	}
+	deviceInfo, err := a.deviceLogic.RegisterDevice(ctx, req.GetDeviceNo())
+	if err != nil {
+		slog.ErrorContext(ctx, "Failed to register device", slog.Any("error", err))
+		return nil, err
+	}
+
+	rsp = &devicemanagementplatformapi.RegisterDeviceResponse{
+		Data: &devicemanagementplatformapi.RegisterDeviceResponseData{
+			DeviceInfo: &devicemanagementplatformapi.DeviceInfo{
+				DeviceId:     deviceInfo.Id,
+				DeviceNo:     deviceInfo.DeviceNo,
+				RegisterTime: deviceInfo.RegisterTime.UnixMilli(),
+			},
+		},
+	}
+	slog.InfoContext(ctx, "Successfully registered device", slog.Any("device", deviceInfo))
+	return rsp, nil
 }
